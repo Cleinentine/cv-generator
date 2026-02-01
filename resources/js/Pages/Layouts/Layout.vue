9ddb168c204f6ac8742e9dbddcf0daab20250714
Cleinentine <cleinentine@gmail.com>
2026-02-01 11:18:29 +0800

    1.1 - Layouts, Routing, Links,  Progress Indicator, and Ziggy

---
+++ resources/js/Pages/Layouts/Layout.vue
@@ -0,0 +1,66 @@
+<script setup></script>
+
+<template>
+    <aside
+        class="bg-slate-900 fixed h-screen py-5 text-center text-white w-[4%]"
+    >
+        <nav>
+            <ul>
+                <li>
+                    <Link
+                        class="block duration-500 hover:bg-green-600 p-5 text-2xl"
+                        :href="route('home')"
+                    >
+                        <span><i class="fa-solid fa-house"></i></span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link
+                        class="block duration-500 hover:bg-green-600 p-5 text-2xl"
+                        :href="route('login')"
+                    >
+                        <span><i class="fa-solid fa-user-plus"></i></span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link
+                        class="block duration-500 hover:bg-green-600 p-5 text-2xl"
+                        :href="route('forgot')"
+                    >
+                        <span><i class="fa-solid fa-paper-plane"></i></span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link
+                        class="block duration-500 hover:bg-green-600 p-5 text-2xl"
+                        :href="route('contact')"
+                    >
+                        <span><i class="fa-solid fa-address-book"></i></span>
+                    </Link>
+                </li>
+
+                <li>
+                    <Link
+                        class="block duration-500 hover:bg-green-600 p-5 text-2xl"
+                        :href="route('feedback')"
+                    >
+                        <span><i class="fa-solid fa-message"></i></span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </aside>
+
+    <main class="float-right relative w-[96%]">
+        <section class="p-5">
+            <slot />
+        </section>
+
+        <!-- <footer class="bg-slate-900 p-5 text-center text-sm text-green-400">
+            <h2>&copy; 2025. All Rights Reserved.</h2>
+        </footer> -->
+    </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+Route::inertia('/', 'Home')->name('home');
+Route::inertia('/contact', 'Contact')->name('contact');
+Route::inertia('/feedback', 'Feedback')->name('feedback');
+Route::inertia('/forgot', 'Forgot')->name('forgot');
+Route::inertia('/login', 'Login')->name('login');
+Route::inertia('/privacy', 'Privacy')->name('privacy');
+Route::inertia('/register', 'Register')->name('register');
+Route::inertia('/terms', 'Terms')->name('terms');
